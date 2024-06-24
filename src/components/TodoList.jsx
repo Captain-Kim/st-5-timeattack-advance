4089157cc5bfabe8533927177c410d7b11866f8c
Captain-Kim <brighthero@kakao.com>
2024-06-24 16:42:00 +0900

    2번 문제

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
@@ -20,24 +20,54 @@ export default function TodoList() {
 
   // TODO: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
-      queryClient.setQueryData(["todos"], (prev) =>
-        prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
-      );
-      await todoApi.patch(`/todos/${id}`, {
-        liked: !currentLiked,
-      });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
-    }
+
+
+
+  // const handleLike = async (id, currentLiked) => {
+  //   const previousTodos = [...todos];
+  //   try {
+  //     queryClient.setQueryData(["todos"], (prev) =>
+  //       prev.map((todo) =>
+  //         todo.id === id ? { ...todo, liked: !todo.liked } : todo,
+  //       ),
+  //     );
+  //     await todoApi.patch(`/todos/${id}`, {
+  //       liked: !currentLiked,
+  //     });
+  //   } catch (err) {
+  //     console.error(err);
+  //     queryClient.setQueryData(["todos"], previousTodos);
+  //   } finally {
+  //     refetch();
+  //   }
+  // };
+
+  const updateLike = async ({ id, currentLiked }) => {
+    await todoApi.patch(`/todos/${id}`, { liked: !currentLiked });
   };
+
+  useMutation({
+    mutationFn: updateLike,
+    onMutate: async ({ id, currentLiked }) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] })
+      const previosTodos = queryClient.getQueryData(['todos'])
+      queryClient.setQueryData(['todos'], (old) =>
+        old.map((todo) =>
+          todo.id === id ? { ...todo, liked: !todo.liked} : todo))
+
+      return { previosTodos }
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(['todos'], context.previosTodos)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+  })
+
+  const handleLike = (id, currentLiked) => {
+    mutation.mutate({ id, currentLiked })
+  }
 
   if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
