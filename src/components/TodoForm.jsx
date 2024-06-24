3a42224579b187652847b7bcaa391eef5ccb9d69
Captain-Kim <brighthero@kakao.com>
2024-06-24 15:54:37 +0900

    1번 문제

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,24 +1,51 @@
 import { useState } from "react";
 import { todoApi } from "../api/todos";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function TodoForm({ fetchData }) {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
+
   // TODO: useMutation 으로 리팩터링 하세요.
-  const handleAddTodo = async (e) => {
+  // const handleAddTodo = async (e) => {
+  //   e.preventDefault();
+  //   setTitle("");
+  //   setContents("");
+  //   await todoApi.post("/todos", {
+  //     id: Date.now().toString(),
+  //     title,
+  //     contents,
+  //     isCompleted: false,
+  //     createdAt: Date.now(),
+  //   });
+  //   await fetchData();
+  // };
+
+  // const queryClient = new useQueryClient();
+
+  const addTodo = async (newTodo) => {
+    await todoApi.post('/todos', newTodo)
+  }
+
+  const mutation = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      alert('등록 성공');
+      queryClient.invalidateQueries(['todos']);
+    }
+  });
+
+  const handleAddTodo = (e) => {
     e.preventDefault();
-    setTitle("");
-    setContents("");
-    await todoApi.post("/todos", {
-      id: Date.now().toString(),
+    mutation.mutate({
       title,
-      contents,
-      isCompleted: false,
-      createdAt: Date.now(),
-    });
-    await fetchData();
-  };
+      contents
+    })
+    setTitle('');
+    setContents('');
+  }
 
   return (
     <form onSubmit={handleAddTodo}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,33 +3,46 @@ import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Home() {
   // TODO: useQuery 로 리팩터링 하세요.
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await todoApi.get("/todos");
-      setData(response.data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await todoApi.get("/todos");
+  //     setData(response.data);
+  //   } catch (err) {
+  //     setError(err);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
 
-  if (isLoading) {
+  // }, []);
+
+  const fetchTodos = async () => {
+    const response = await todoApi.get('/todos');
+    return response.data;
+  }
+
+  const { data: todos, isPending, isError, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos
+  });
+
+  if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
   }
 
-  if (error) {
+  if (isError) {
     console.error(error);
     return (
       <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
@@ -39,8 +52,8 @@ export default function Home() {
   return (
     <>
       <h2>서버통신 투두리스트 by useState</h2>
-      <TodoForm fetchData={fetchData} />
-      <TodoList todos={data} />
+      <TodoForm  />
+      <TodoList todos={todos} />
     </>
   );
 }
